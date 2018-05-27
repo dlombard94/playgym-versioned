a2b6f329e36eda4820cc0ab54c0c39436c150b18
DLOMBARD\Darragh <112500343@umail.ucc.ie>
2018-05-27 13:49:11 +0100

    added Trainer model

---
+++ app/models/Trainer.java
@@ -0,0 +1,33 @@
+package models;
+
+import play.db.jpa.Model;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Trainer extends Model
+{
+    public String email;
+    public String password;
+
+    public Trainer(String email, String password)
+    {
+        this.email = email;
+        this.password = password;
+    }
+
+    public static Trainer findByEmail(String email)
+    {
+        return find("email", email).first();
+    }
+
+    public boolean checkPassword(String password)
+    {
+        return this.password.equals(password);
+    }
+}
+
