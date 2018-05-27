85659b0c71a4fb16eedb4f8a72c75b224d83322c
DLOMBARD\Darragh <112500343@umail.ucc.ie>
2018-05-27 13:11:00 +0100

    added routes and controller methods to allow members to add and delete assessments

--- app/controllers/Dashboard.java
+++ app/controllers/Dashboard.java
@@ -15,4 +15,25 @@ public class Dashboard extends Controller
     List<Assessment> assessments = member.assessments;
     render ("dashboard.html",member,assessments);
   }
+
+  public static void addAssessment(double weight, double chest, double thigh,double upperarm, double waist, double hips){
+    Member member = Accounts.getLoggedInMember();
+    Assessment assessment = new Assessment(weight,chest,thigh,upperarm,waist,hips);
+    member.assessments.add(assessment);
+    member.save();
+    redirect ("/dashboard");
+  }
+
+  public static void deleteAssessment(Long id, Long assessmentid){
+    Member member = Member.findById(id);
+    Assessment assessment = Assessment.findById(assessmentid);
+    Logger.info("Removing "+assessment);
+    member.assessments.remove(assessment);
+    member.save();
+    assessment.delete();
+
+    redirect("/dashboard");
+  }
+
+
 }
