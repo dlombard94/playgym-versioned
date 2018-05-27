9742192bed25d1be85e5377ab68c8a2f4edee79d
DLOMBARD\Darragh <112500343@umail.ucc.ie>
2018-05-27 12:10:07 +0100

    updated dashboard controller to get all members and pass them to the dashboard view

--- app/controllers/Dashboard.java
+++ app/controllers/Dashboard.java
@@ -1,12 +1,16 @@
 package controllers;
 
+import models.Member;
 import play.Logger;
 import play.mvc.Controller;
+
+import java.util.List;
 
 public class Dashboard extends Controller
 {
   public static void index() {
     Logger.info("Rendering Dashboard");
-    render ("dashboard.html");
+    List<Member> members = Member.findAll();
+    render ("dashboard.html",members);
   }
 }
